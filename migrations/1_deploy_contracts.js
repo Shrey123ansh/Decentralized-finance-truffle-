ad3555fb656a26300476732e822a07b1eb530320
Shrey123ansh <shreyann56sh@gmail.com>
2023-06-03 02:53:14 +0530

    first commit

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,11 @@
-const Lottery = artifacts.require("./Lottery.sol");
+// const Lottery = artifacts.require("./Lottery.sol");
+// const IERC20 = artifacts.require("./IERC20.sol");
+// const IUniswapV2Router = artifacts.require("./IUniswapV2Router.sol");
+const TestUniswap = artifacts.require("./TestUniswap.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Lottery);
+  // deployer.deploy(Lottery);
+  // deployer.deploy(IERC20);
+  // deployer.deploy(IUniswapV2Router);
+  deployer.deploy(TestUniswap);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -33,19 +33,26 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-  contracts_build_directory: "./client/src/contracts",
+  // contracts_build_directory: "./client/src/contracts",
   networks: {
+    // mainnet_fork: {
+    //   host: "127.0.0.1", // Localhost (default: none)
+    //   port: 7545, // Standard Ethereum port (default: none)
+    //   network_id: "*", // Any network (default: none)
+    // },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
+    ///////////////////////
     development: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 7545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
+    ////////////////////////
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -81,7 +88,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.9", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
